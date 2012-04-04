5c57dbe1ad717fb95c2319ceb599946493a22ebe
minhkhanhhaugiang@gmail.com <minhkhanhhaugiang@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-04-04 09:22:51 +0000

    update lai template project moi

---
+++ documents/LocalServer/App_Code/ILocalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract]
+public interface ILocalService
+{
+    [WebInvoke(Method = "GET", UriTemplate = "cong?a={a}&b={b}")]
+    [OperationContract]
+    int PhepCong(int a, int b);
+    [WebInvoke(Method = "GET", UriTemplate = "test")]
+    [OperationContract]
+    string Test();
+}
---
+++ documents/LocalServer/App_Code/LocalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+public class LocalService : ILocalService
+{
+    public int PhepCong(int a, int b)
+    {
+        return a + b;
+    }
+
+
+    public string Test()
+    {
+        return "Hello eMenu";
+    }
+}
