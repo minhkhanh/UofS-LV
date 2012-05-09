7b111fe7466c1edecbf1ebc51516cbc1af170537
onmyway133@gmail.com <onmyway133@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-05-09 04:23:04 +0000

    Begin: Hien danh sach danh muc

---
+++ localserver/LocalServerWeb/Controllers/AdminCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using LocalServerBUS;
+using LocalServerWeb.Codes;
+using LocalServerWeb.Resources.Views.AdminExchangeRate;
+using LocalServerWeb.Resources.Views.Shared;
+using LocalServerDTO;
+using LocalServerWeb.ViewModels;
+
+namespace LocalServerWeb.Controllers
+{
+    public class AdminCategoryController : BaseController
+    {
+        public ActionResult Index()
+        {
+            SharedCode.FillAdminMainMenu(ViewData, 3, 0);
+            ViewData["listTiGia"] = TiGiaBUS.LayDanhSachTiGiaThatSu();
+            return View();
+        }
+    }
+}
