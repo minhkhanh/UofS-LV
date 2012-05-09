7e92bd6f0fe87bceb3f81ca016cca4f7bf063d3c
onmyway133@gmail.com <onmyway133@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-05-09 03:03:54 +0000

    Them Ti Gia Sua Ti Gia cap nhat CSDL 0.7 (Bang Ti Gia, record VND phai la first)

--- localserver/LocalServerWeb/Controllers/AdminExchangeRateController.cs
+++ localserver/LocalServerWeb/Controllers/AdminExchangeRateController.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using LocalServerBUS;
 using LocalServerWeb.Codes;
-using LocalServerWeb.Resources.Views.AdminTable;
+using LocalServerWeb.Resources.Views.AdminExchangeRate;
 using LocalServerWeb.Resources.Views.Shared;
 using LocalServerDTO;
 using LocalServerWeb.ViewModels;
@@ -17,9 +17,193 @@ namespace LocalServerWeb.Controllers
     {
         public ActionResult Index()
         {
-            SharedCode.FillAdminMainMenu(ViewData, 3, 6);
-            ViewData["listTiGia"] = TiGiaBUS.LayDanhSachTiGia();
+            SharedCode.FillAdminMainMenu(ViewData, 3,8);
+            ViewData["listTiGia"] = TiGiaBUS.LayDanhSachTiGiaThatSu();
             return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                TempData["error"] = SharedString.InputWrong;
+                return RedirectToAction("Index", "Error");
+            }
+
+            TiGia objTiGia = TiGiaBUS.LayTiGia(id ?? 0);
+            if (objTiGia == null)
+            {
+                TempData["error"] = SharedString.InputWrong;
+                return RedirectToAction("Index", "Error");
+            }
+
+            if (!TiGiaBUS.Xoa(objTiGia.MaTiGia))
+            {
+                TempData["errorCannotDelete"] = AdminExchangeRateString.ErrorCannotDelete;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Add()
+        {
+            SharedCode.FillAdminMainMenu(ViewData, 3, 9);
+
+            if (TempData["checkDic"] == null)
+            {
+                TempData.Clear();
+                TempData["checkDic"] = new Dictionary<string, string>();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(string kiHieu, string giaTri)
+        {
+            TempData["kiHieu"] = kiHieu;
+            TempData["giaTri"] = giaTri;
+
+            var checkDic = new Dictionary<string, string>();
+
+            bool bCheckOk = true;
+
+            if (kiHieu == null || kiHieu.Trim().Length < 1)
+            {
+                bCheckOk = false;
+                checkDic.Add("kiHieu", SharedString.InputWrong);
+            }
+
+            float _giaTri = 0;
+            float.TryParse(giaTri, out _giaTri);
+            if (_giaTri <= 0)
+            {
+                bCheckOk = false;
+                checkDic.Add("giaTri", SharedString.InputWrong);
+            }
+
+            if (bCheckOk)
+            {
+                try
+                {
+                    TiGia tiGia = new TiGia();
+                    tiGia.KiHieu = kiHieu;
+                    tiGia.GiaTri = _giaTri;
+                    tiGia.ThoiDiemCapNhat = DateTime.Now;
+
+
+                    // Need to clear TempData
+                    if (TiGiaBUS.Them(tiGia))
+                        return RedirectToAction("Index", "AdminExchangeRate");
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine(e.StackTrace);
+                }
+            }
+
+            TempData["checkDic"] = checkDic;
+            return RedirectToAction("Add");
+
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            SharedCode.FillAdminMainMenu(ViewData, 3, 9);
+
+            if (TempData["checkDic"] == null)
+            {
+                TempData.Clear();
+                TempData["checkDic"] = new Dictionary<string, string>();
+            }
+
+            TiGia objTiGia = TiGiaBUS.LayTiGia(id ?? 0);
+            if (id == null || objTiGia == null)
+            {
+                TempData["errorNotFound"] = AdminExchangeRateString.ErrorExchangeRateNotFound;
+            }
+            else
+            {
+                TempData["kiHieu"] = objTiGia.KiHieu;
+                TempData["giaTri"] = objTiGia.GiaTri.ToString();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int maTiGia, string kiHieu, string giaTri)
+        {
+            TempData["kiHieu"] = kiHieu;
+            TempData["giaTri"] = giaTri;
+
+            var checkDic = new Dictionary<string, string>();
+
+            bool bCheckOk = true;
+
+            if (kiHieu == null || kiHieu.Trim().Length < 1)
+            {
+                bCheckOk = false;
+                checkDic.Add("kiHieu", SharedString.InputWrong);
+            }
+
+            float _giaTri = 0;
+            float.TryParse(giaTri, out _giaTri);
+            if (_giaTri <= 0)
+            {
+                bCheckOk = false;
+                checkDic.Add("giaTri", SharedString.InputWrong);
+            }
+
+            if (bCheckOk)
+            {
+                try
+                {
+                    TiGia tiGia = TiGiaBUS.LayTiGia(maTiGia);
+                    tiGia.KiHieu = kiHieu;
+                    tiGia.GiaTri = _giaTri;
+                    tiGia.ThoiDiemCapNhat = DateTime.Now;
+
+
+                    // Need to clear TempData
+                    if (TiGiaBUS.CapNhat(tiGia))
+                        return RedirectToAction("Index", "AdminExchangeRate");
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine(e.StackTrace);
+                }
+            }
+
+            TempData["checkDic"] = checkDic;
+            return RedirectToAction("Add");
+
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                TempData["error"] = SharedString.InputWrong;
+                return RedirectToAction("Index", "Error");
+            }
+
+            TiGia objTiGia = TiGiaBUS.LayTiGia(id ?? 0);
+            if (objTiGia == null)
+            {
+                TempData["error"] = SharedString.InputWrong;
+                return RedirectToAction("Index", "Error");
+            }
+
+            // Need to GET from vnexpress.net
+            objTiGia.GiaTri = 333;
+
+
+            if (!TiGiaBUS.CapNhat(objTiGia))
+            {
+                TempData["errorCannotUpdate"] = AdminExchangeRateString.ErrorCannotUpdate;
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
