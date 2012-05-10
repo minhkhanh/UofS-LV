909cc0831d512d900b514bf1780e856090f83779
onmyway133@gmail.com <onmyway133@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-05-10 02:50:04 +0000

    ChiTietCheBienOrder DAO, BUS

---
+++ localserver/LocalServerBUS/ChiTietCheBienOrderBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LocalServerDAO;
+using LocalServerDTO;
+
+namespace LocalServerBUS
+{
+    public class ChiTietCheBienOrderBUS
+    {
+        public static ChiTietCheBienOrder LayChiTietCheBienOrder(int maChiTietOrder)
+        {
+            return ChiTietCheBienOrderDAO.LayChiTietCheBienOrder(maChiTietOrder);
+        }
+
+        public static bool ThemChiTietCheBienOrder(ChiTietCheBienOrder _chiTietCheBienOrder)
+        {
+            return ChiTietCheBienOrderDAO.ThemChiTietCheBienOrder(_chiTietCheBienOrder);
+        }
+
+        public static bool SuaChiTietCheBienOrder(ChiTietCheBienOrder _chiTietCheBienOrder)
+        {
+            return ChiTietCheBienOrderDAO.SuaChiTietCheBienOrder(_chiTietCheBienOrder);
+        }
+    }
+}
---
+++ localserver/LocalServerDAO/ChiTietCheBienOrderDAO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LocalServerDTO;
+
+namespace LocalServerDAO
+{
+    public class ChiTietCheBienOrderDAO
+    {
+        public static ChiTietCheBienOrder LayChiTietCheBienOrder(int maChiTietOrder)
+        {
+            var temp = ThucDonDienTu.DataContext.ChiTietCheBienOrders.Where(c => c.ChiTietOrder.MaChiTietOrder == maChiTietOrder);
+            if (temp.Count() > 0)
+            {
+                ChiTietCheBienOrder or = temp.First();
+                return or;
+            }
+            return null;
+        }
+
+        public static bool ThemChiTietCheBienOrder(ChiTietCheBienOrder _chiTietCheBienOrder)
+        {
+            bool result = false;
+            ThucDonDienTu.DataContext.ChiTietCheBienOrders.InsertOnSubmit(_chiTietCheBienOrder);
+            try
+            {
+                ThucDonDienTu.DataContext.SubmitChanges();
+                result = true;
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public static bool SuaChiTietCheBienOrder(ChiTietCheBienOrder _chiTietCheBienOrder)
+        {
+            bool result = false;
+            var temp = ThucDonDienTu.DataContext.ChiTietCheBienOrders.Where(c => c.ChiTietOrder.MaChiTietOrder == _chiTietCheBienOrder._maChiTietOrder);
+            if (temp.Count() > 0)
+            {
+                ChiTietCheBienOrder ct = temp.First();
+                ct._maChiTietOrder = _chiTietCheBienOrder._maChiTietOrder;
+                ct.SoLuongDaCheBien = _chiTietCheBienOrder.SoLuongDaCheBien;
+                ct.SoLuongDangCheBien = _chiTietCheBienOrder.SoLuongDangCheBien;
+            }
+
+            try
+            {
+                ThucDonDienTu.DataContext.SubmitChanges();
+                result = true;
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
