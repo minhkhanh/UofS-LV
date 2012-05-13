81dcffab97b0af226a6d4eb01961997355464b5b
onmyway133@gmail.com <onmyway133@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-05-13 10:20:06 +0000

    begin: bo phan che bien

---
+++ trunk/localserver/LocalServerWeb/Controllers/AdminProcessorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using LocalServerBUS;
+using LocalServerWeb.Codes;
+using LocalServerWeb.Resources.Views.AdminProcessor;
+using LocalServerWeb.Resources.Views.Shared;
+using LocalServerDTO;
+using LocalServerWeb.ViewModels;
+
+namespace LocalServerWeb.Controllers
+{
+    public class AdminProcessorController : BaseController
+    {
+
+    }
+}
