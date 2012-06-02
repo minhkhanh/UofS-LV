d08a064d2e30d03d74867e17c70fa2aed3137d2a
onmyway133@gmail.com <onmyway133@gmail.com@a7da0f68-36ed-d0f3-52d0-80ca023f1efe>
2012-06-02 16:20:39 +0000

    WCF service + sua ham Them Nhieu Chi Tiet Hoa Don: khi thanh toan xong, doi Tinh Trang cua CT Order thanh 4

--- localserver/LocalServerBUS/ChiTietHoaDonBUS.cs
+++ localserver/LocalServerBUS/ChiTietHoaDonBUS.cs
@@ -27,7 +27,21 @@ namespace LocalServerBUS
 
         public static List<ChiTietHoaDon> ThemNhieuChiTietHoaDon(List<ChiTietHoaDon> _listChiTietHoaDon)
         {
-            return ChiTietHoaDonDAO.ThemNhieuChiTietHoaDon(_listChiTietHoaDon);
+            // Khi thanh toan xong, thay doi TinhTrang = 4 cua cac ct Order tuong ung
+            if (ChiTietHoaDonDAO.ThemNhieuChiTietHoaDon(_listChiTietHoaDon) != null)
+            {
+                int maBan = 0;
+                if(_listChiTietHoaDon.Count > 0)
+                    maBan = _listChiTietHoaDon[0].HoaDon.Ban.MaBan;
+                List<ChiTietOrder> listChiTietOrder = ChiTietOrderBUS.LayNhieuChiTietOrderChuaThanhToan(maBan);
+                foreach (ChiTietOrder ctOrder in listChiTietOrder)
+                {
+                    ctOrder.TinhTrang = 4;
+                    ChiTietOrderBUS.SuaChiTietOrder(ctOrder);
+                }
+            }
+
+            return _listChiTietHoaDon;
         }
 
         public static bool SuaChiTietHoaDon(ChiTietHoaDon _chiTietHoaDon)
